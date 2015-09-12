12468a9cade5abb309ddbcf4492db22008144a56
markedwardmurray <markedwardmurray@gmail.com>
2015-09-11 22:28:53 -0400

    Initial Commit

---
+++ objc-fizzbuzz/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc-fizzbuzz
+//
+//  Created by iOS Staff on 9/11/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
